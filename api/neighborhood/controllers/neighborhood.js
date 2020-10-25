ee72d1f280a6835f9114e7d68e030d1f8f8457b3
Gabester0 <gceipper@gmail.com>
2020-10-25 02:37:25 -0400

    created a populate function

--- api/neighborhood/controllers/neighborhood.js
+++ api/neighborhood/controllers/neighborhood.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    populate: async (ctx)=>{
+        const defaultHood = {
+            "Name": "Suspicious Of Strangers",
+            "City": "Boca Raton",
+            "State": "FL",
+            "OrgName": "NeighborhoodWatch",
+            "houses": ["1432 W. Elm St.", "1325 W. Elm St.", "1200 E. Elm St.", "304 Briar Ave."]
+        }
+        const defaultStory = {
+            "photoLink": "https://storage.cloud.google.com/goats-hackathon-2020/IMG_20200317_121038108_HDR.jpg?authuser=1",
+            "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?"
+        }
+        const hood = await strapi.services.neighborhood.create(defaultHood, 'neighborhood')
+        const story = await strapi.services.neighborhood.create(defaultStory, 'stories')
+        return {hood, story}
+    }
+};
